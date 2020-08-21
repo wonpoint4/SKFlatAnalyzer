a168b2a24bc9408cb86b1c235cd850fe29c5526d
jbhyun <jbhyun@cern.ch>
2020-08-21 11:46:34 +0900

    v200821 - update shared gen-matching code

--- Analyzers/include/AnalyzerCore.h
+++ Analyzers/include/AnalyzerCore.h
@@ -186,7 +186,10 @@ public:
   vector<int> TrackGenSelfHistory(const Gen& me, const std::vector<Gen>& gens);
   bool IsFromHadron(const Gen& me, const std::vector<Gen>& gens);
   int GetLeptonType(const Lepton& lep, const std::vector<Gen>& gens);
+  int GetLeptonType_Public(int TruthIdx, const std::vector<Gen>& TruthColl);
   int GetGenPhotonType(const Gen& genph, const std::vector<Gen>& gens);
+  bool IsFinalPhotonSt23_Public(const std::vector<Gen>& TruthColl);
+  int  GetPrElType_InSameSCRange_Public(int TruthIdx, const std::vector<Gen>& TruthColl);
   bool IsSignalPID(int pid);
 
   //==== Plotting
